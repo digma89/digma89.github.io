049f2e7396010a90dc184b33d121097f3e9d8642
Diego Rodriguez <digma89@hotmail.com>
2015-05-26 10:52:06 -0400

    V1.1

---
+++ The_Lost_Sword1/scripts/game.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="typings/impress/impress.d.ts" />
+
+
+//Inicialize you framework-- >
+function init() {
+    console.log("Game is running"); //Console message to see if the game is running   
+     impress().init();
+} 
+
+
